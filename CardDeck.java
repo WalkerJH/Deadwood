263b4c549dbf5c129aeb0881e565f2451c47c7c4
Walker <herrinw@wwu.edu>
2019-11-10 15:01:39 -0800

    Added constructor that randomizes the order of cards

--- CardDeck.java
+++ CardDeck.java
@@ -3,15 +3,21 @@
  */
 
 import java.util.Stack;
+import java.util.Collections;
 
 public class CardDeck {
     private static Stack<Card> deck;
 
-    public CardDeck() {
-
+    public CardDeck(Card ... args) {
+        for (int i = 0; i < args.length; i++) {
+            deck.push(args[i]);
+        }
+        Collections.shuffle(deck);
     }
 
     public Card drawCard(){
         return deck.pop();
     }
+
+    public void addCard(Card c) { deck.push(c); }
 }
