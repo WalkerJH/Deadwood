b33d207667b4ff197c61d1b22babcbbe4dcd06d0
Kai Broach <broachk@wwu.edu>
2019-11-19 12:50:41 -0800

    GUI frame sts its size

--- DeadwoodGUI.java
+++ DeadwoodGUI.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class DeadwoodGUI {
     public DeadwoodGUI() {
         JFrame frame = new JFrame("Deadwood");
+        frame.setSize(1920, 1080);
         frame.setVisible(true);
     }
 }
