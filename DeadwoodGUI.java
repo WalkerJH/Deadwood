d09279d8491b4cb0dabb88623972f3bc38b1e8f9
Walker <herrinw@wwu.edu>
2019-11-24 21:50:27 -0800

    Display board in the window

--- DeadwoodGUI.java
+++ DeadwoodGUI.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.imageio.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 /**
  * Displays GUI for a Deadwood game run through Deadwood.java
@@ -8,6 +12,22 @@ public class DeadwoodGUI {
     public DeadwoodGUI() {
         JFrame frame = new JFrame("Deadwood");
         frame.setSize(1920, 1080);
+        JLabel boardLabel = new JLabel();
+        boardLabel.setIcon(new ImageIcon(getImage("board.jpg")));
+        frame.add(boardLabel);
+
         frame.setVisible(true);
+    }
+
+    public BufferedImage getImage(String fileName) {
+        BufferedImage image = null;
+        File file = new File(fileName);
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return image;
     }
 }
