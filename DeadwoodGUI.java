8c07ac48a61dc58e9c36b63ba6629931be91c370
Kai Broach <broachk@wwu.edu>
2019-12-03 19:04:26 -0800

    Added buttons

--- DeadwoodGUI.java
+++ DeadwoodGUI.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import javax.imageio.*;
-import java.awt.Image;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
@@ -10,13 +10,26 @@ import java.io.*;
  */
 public class DeadwoodGUI {
     private JFrame frame;
+    private JLayeredPane pane;
+    private MouseListener listener;
+    private JButton moveButton;
+    private JButton actButton;
+    private JButton rehearseButton;
+    private int boardWidth;
+    private int boardHeight;
 
     public DeadwoodGUI() {
-        JFrame frame = new JFrame("Deadwood");
-        frame.setSize(1920, 1080);
+        frame = new JFrame("Deadwood");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pane = frame.getLayeredPane();
         JLabel boardLabel = new JLabel();
         boardLabel.setIcon(new ImageIcon(getImage("board.jpg")));
-        frame.add(boardLabel);
+        boardWidth = boardLabel.getIcon().getIconWidth();
+        boardHeight = boardLabel.getIcon().getIconHeight();
+        boardLabel.setBounds(0, 0, boardWidth, boardHeight);
+        frame.setSize(boardWidth + 300, boardHeight + 50);
+        pane.add(boardLabel, new Integer(0));
+        setUpButtons();
 
         frame.setVisible(true);
     }
@@ -33,6 +46,13 @@ public class DeadwoodGUI {
         return image;
     }
 
+   public void displayException(Exception ex) {
+       JOptionPane.showMessageDialog(frame, ex,"Error", JOptionPane.WARNING_MESSAGE);
+       System.exit(-1);
+       frame.dispose();
+       frame.setVisible(false);
+   }
+
     public int promptNumPlayers() {
         try {
             return Integer.parseInt(JOptionPane.showInputDialog(frame, "Please enter number of Players",
@@ -42,12 +62,23 @@ public class DeadwoodGUI {
             displayException(ex);
             return -1;
         }
-   }
+    }
 
-   public void displayException(Exception ex) {
-       JOptionPane.showMessageDialog(frame, ex,"Error", JOptionPane.WARNING_MESSAGE);
-       System.exit(-1);
-       frame.dispose();
-       frame.setVisible(false);
-   }
+    private void setUpButtons() {
+        moveButton = new JButton("Move");
+        moveButton.setBounds(boardWidth + 50, 10, 200, 100);
+        pane.add(moveButton, new Integer(1));
+
+        actButton = new JButton("Act");
+        actButton.setBounds(boardWidth + 50, 120, 200, 100);
+        actButton.setVisible(false);
+        pane.add(actButton, new Integer(1));
+
+        rehearseButton = new JButton("Rehearse");
+        rehearseButton.setBounds(boardWidth + 50, 230, 200, 100);
+        rehearseButton.setVisible(false);
+        pane.add(rehearseButton, new Integer(1));
+
+
+    }
 }
